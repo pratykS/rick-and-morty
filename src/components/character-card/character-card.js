c908b7ff5223fbf9f869655956652a13391bb3f4
Sah <pratik.sah@here.com>
2019-11-11 01:17:22 +0530

    style changes and addition of attributes like type,origin,id

--- src/components/character-card/character-card.js
+++ src/components/character-card/character-card.js
@@ -23,21 +23,22 @@ export class CharacterCard extends React.Component{
     render(){
 
 	const episodes = this.props.chapter;
-	const {image,name,species,status,location,episode} = this.props.character;
-	
+	const {image,name,species,status,location,episode,origin,type,id} = this.props.character;
 	const episodeFeatured = episode.map(function(eps){
 		return episodes[eps];
 	});
 
     return (
             <div className="character">
+			<span className="charId">{id}</span>
               <div className="character-content">
               <img src={image} alt="character" />
                 <h3>Name: {name}</h3>
-                <h4>Species: {species}</h4>
+				<h3>Origin: {origin.name}</h3>
+				<h4>Species: {species}{type.length>0?', ':null}{type}</h4>
                 <h4>Status: {status}</h4>
                 <h4>Location: {location.name}</h4>
-				<p>{name} featured in <span className="episodeCount" onClick={this.handleClick}>{episodeFeatured.length}</span>
+				<p>{name} featured in <span className="episodeCount" onClick={this.handleClick} title="Click to see the episodes featured">{episodeFeatured.length}</span>
 						{
 							episodeFeatured.length > 1 ? ' Chapters' : ' Chapter'
 						}
